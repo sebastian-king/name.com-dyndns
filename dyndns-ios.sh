05f25e429f7b3d8e45340ac1e3a33673424c7a23
Massivesoft <admin@massivesoft.net>
2015-08-31 19:33:14 -0500

    file for dynamically setting an iphone's inet address
    the address 172.20.10.2 may be a static ip set by the host i don't know will need investigation because standard localhost still exists

---
+++ dyndns-ios.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+        echo "Usage: $0 <super_domain> <sub_domain>";
+        exit
+fi
+
+SUPER_DOMAIN="${1}";
+SUB_DOMAIN="${2}";
+
+ip=`ifconfig en0 | egrep -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" | head -n 1`;
+if [ -z "$ip" ]; then
+  ip="172.20.10.2";
+fi
+wget "http://api.massivesoft.net/api/ip2host.php?sub_domain=${SUB_DOMAIN}&super_domain=${SUPER_DOMAIN}&ip=$ip" -qO- && echo ""
