c600e7980c6937244615c17a858696e3866a2d36
Sebastian King <sebastian.king@btinternet.com>
2015-09-23 14:33:45 -0500

    removed my api hostname for security reasons

--- dyndns-ios.sh
+++ dyndns-ios.sh
@@ -5,6 +5,8 @@ if [ "$#" -ne 2 ]; then
         exit
 fi
 
+API_HOST="api.example.net";
+
 SUPER_DOMAIN="${1}";
 SUB_DOMAIN="${2}";
 
@@ -12,4 +14,4 @@ ip=`ifconfig en0 | egrep -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" | head -n 1`;
 if [ -z "$ip" ]; then
   ip="172.20.10.2"; // my public ip, don't know where this comes from
 fi
-wget "http://api.massivesoft.net/api/ip2host.php?sub_domain=${SUB_DOMAIN}&super_domain=${SUPER_DOMAIN}&ip=$ip" -qO- && echo ""
+wget "http://${API_HOST}/api/ip2host.php?sub_domain=${SUB_DOMAIN}&super_domain=${SUPER_DOMAIN}&ip=$ip" -qO- && echo ""
