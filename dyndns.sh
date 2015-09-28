e4dd354e98b24562b4dff289c17aa0a66c7df7ac
Sebastian King <sebastian.king@btinternet.com>
2015-09-28 15:22:37 -0500

    Update dyndns.sh

--- dyndns.sh
+++ dyndns.sh
@@ -11,4 +11,5 @@ SUPER_DOMAIN="${1}";
 SUB_DOMAIN="${2}";
 
 ip=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`;
-wget "http://${API_HOST}/api/ip2host.php?sub_domain=${SUB_DOMAIN}&super_domain=${SUPER_DOMAIN}&ip=$ip" -qO- && echo ""
+ttl=60;
+wget "http://${API_HOST}/api/ip2host.php?sub_domain=${SUB_DOMAIN}&super_domain=${SUPER_DOMAIN}&ip=$ip&ttl=$ttl" -qO- && echo ""
