dd209cab8d4cd6915d68575810866aa74b3cb002
Sebastian King <sebastian.king@btinternet.com>
2015-10-28 01:08:37 -0500

    Update dyndns-ios.sh

--- dyndns-ios.sh
+++ dyndns-ios.sh
@@ -7,15 +7,18 @@ if [ "$#" -ne 2 ]; then
         exit
 fi
 
-API_HOST="api.example.net";
+API_HOST="api.massivesoft.net";
 
 SUPER_DOMAIN="${1}";
 SUB_DOMAIN="${2}";
 
 ip=`ifconfig en0 | egrep -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" | head -n 1`;
-ttl=60;
-if [ -z "$ip" ]; then
-  ip="";
-  #ip=`wget http://${API_HOST}/api/ip.php -qO- --no-check-certificate | head -n 1`; // integrated into ip2host.php
-fi
+
+ttl=60; # 1 minute becuase Mobile Internet to WiFi change can be very quick
+
+#if [ -z "$ip" ]; then # maybe this is obsolete due to the new internal handling of a null IP
+#  ip="";
+#  #ip=`wget http://${API_HOST}/api/ip.php -qO- --no-check-certificate | head -n 1`; // integrated into ip2host.php
+#fi
+
 wget "http://${API_HOST}/api/ip2host.php?sub_domain=${SUB_DOMAIN}&super_domain=${SUPER_DOMAIN}&ip=$ip&ttl=$ttl" -qO- --no-check-certificate && echo ""
