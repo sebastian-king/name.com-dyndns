74857ef2b70d014a471691d9ef29cfc7913f16be
root <root@x.massivesoft.net>
2017-04-30 07:50:18 +0200

    further improved program flow to avoid die or goto

--- ip2host.php
+++ ip2host.php
@@ -24,32 +24,32 @@ if (!isset($super_domain)) {
 
 		if ($response->result->code != 100) {
 		        $a = array("response"=>"An unknown error has occured when querying the superdomain.","code"=>$response->result->code);
-		}
-
-		foreach ($response->records as $key => $val) {
-		        if ($val->name == $sub_domain.".".$super_domain) { // delete entry
-		                $_response = $api->delete_dns_record($super_domain, $val->record_id);
-		                if ($_response->result->code != 100) {
-		                        $a = array("response"=>"An unknown error has occured when deleting the old entry.","code"=>$_response->result->code);
-					break;
-		                }
-		        }
-		}
-
-		if (empty($a)) {
-			if (!filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6) === false) {
-			        // $ip is valid IPv6
-			        $record_type = "AAAA";
-			} else {
-			        $record_type = "A";
+		} else {
+			foreach ($response->records as $key => $val) {
+			        if ($val->name == $sub_domain.".".$super_domain) { // delete entry
+			                $_response = $api->delete_dns_record($super_domain, $val->record_id);
+			                if ($_response->result->code != 100) {
+			                        $a = array("response"=>"An unknown error has occured when deleting the old entry.","code"=>$_response->result->code);
+						break;
+			                }
+			        }
 			}
 
-			$response = $api->create_dns_record($super_domain, $sub_domain, $record_type, $ip, $ttl);
+			if (empty($a)) {
+				if (!filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6) === false) {
+				        // $ip is valid IPv6
+				        $record_type = "AAAA";
+				} else {
+				        $record_type = "A";
+				}
 
-			if ($response->result->code != 100) {
-			        $a = array("response"=>"An unknown error has occured when updating the record.","code"=>$response->result->code);
-			} else {
-			        $a = array("response"=>"IP updated successfully.","code"=>$response->result->code);
+				$response = $api->create_dns_record($super_domain, $sub_domain, $record_type, $ip, $ttl);
+
+				if ($response->result->code != 100) {
+				        $a = array("response"=>"An unknown error has occured when updating the record.","code"=>$response->result->code);
+				} else {
+				        $a = array("response"=>"IP updated successfully.","code"=>$response->result->code);
+				}
 			}
 		}
 	}
