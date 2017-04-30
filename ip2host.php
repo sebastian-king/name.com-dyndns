ab5f9619533fb73e655c5038f62f3df8e403bc3d
root <root@x.massivesoft.net>
2017-04-30 07:47:49 +0200

    fixed program flow to avoid needing die or goto

--- ip2host.php
+++ ip2host.php
@@ -7,47 +7,52 @@ $api->login($api_username, $api_key);
 $sub_domain = $_GET['sub_domain'];
 $super_domain = $_GET['super_domain'];
 
+$a = array();
+
 if (!isset($super_domain)) {
-	die(json_encode(array("response"=>"You must specify a super domain.")));
-}
-
-$ip = ((@$_GET['ip']) ? $_GET['ip'] : $_SERVER['REMOTE_ADDR']);
-$ttl = ((@$_GET['ttl']) ? $_GET['ttl'] : 300); // default 5 minute time to live
-
-$response = $api->get_domain($super_domain);
-
-if ($response->result->code != 100) {
-        $a = array("response"=>"You probably don't own the domain.","code"=>$response->result->code);
-}
-
-$response = $api->list_dns_records($super_domain);
-
-if ($response->result->code != 100) {
-        $a = array("response"=>"An unknown error has occured when querying the superdomain.","code"=>$response->result->code);
-}
-
-foreach ($response->records as $key => $val) {
-        if ($val->name == $sub_domain.".".$super_domain) { // delete entry
-                $_response = $api->delete_dns_record($super_domain, $val->record_id);
-                if ($_response->result->code != 100) {
-                        $a = array("response"=>"An unknown error has occured when deleting the old entry.","code"=>$_response->result->code);
-                }
-        }
-}
-
-if (!filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6) === false) {
-        // $ip is valid IPv6
-        $record_type = "AAAA";
+	$a = array("response"=>"You must specify a super domain.");
 } else {
-        $record_type = "A";
-}
+	$ip = ((@$_GET['ip']) ? $_GET['ip'] : $_SERVER['REMOTE_ADDR']);
+	$ttl = ((@$_GET['ttl']) ? $_GET['ttl'] : 300); // default 5 minute time to live
 
-$response = $api->create_dns_record($super_domain, $sub_domain, $record_type, $ip, $ttl);
+	$response = $api->get_domain($super_domain);
 
-if ($response->result->code != 100) {
-        $a = array("response"=>"An unknown error has occured when updating the record.","code"=>$response->result->code);
-} else {
-        $a = array("response"=>"IP updated successfully.","code"=>$response->result->code);
+	if ($response->result->code != 100) {
+	        $a = array("response"=>"You probably don't own the domain.","code"=>$response->result->code);
+	} else {
+		$response = $api->list_dns_records($super_domain);
+
+		if ($response->result->code != 100) {
+		        $a = array("response"=>"An unknown error has occured when querying the superdomain.","code"=>$response->result->code);
+		}
+
+		foreach ($response->records as $key => $val) {
+		        if ($val->name == $sub_domain.".".$super_domain) { // delete entry
+		                $_response = $api->delete_dns_record($super_domain, $val->record_id);
+		                if ($_response->result->code != 100) {
+		                        $a = array("response"=>"An unknown error has occured when deleting the old entry.","code"=>$_response->result->code);
+					break;
+		                }
+		        }
+		}
+
+		if (empty($a)) {
+			if (!filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6) === false) {
+			        // $ip is valid IPv6
+			        $record_type = "AAAA";
+			} else {
+			        $record_type = "A";
+			}
+
+			$response = $api->create_dns_record($super_domain, $sub_domain, $record_type, $ip, $ttl);
+
+			if ($response->result->code != 100) {
+			        $a = array("response"=>"An unknown error has occured when updating the record.","code"=>$response->result->code);
+			} else {
+			        $a = array("response"=>"IP updated successfully.","code"=>$response->result->code);
+			}
+		}
+	}
 }
 
 echo json_encode($a);
